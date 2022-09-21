e64e3cbd8e77dfcd2c0b1114010af0bfb3d29cef
Nebiyou Yonas Metaferia <nebsdivo@gmail.com>
2022-09-21 16:01:03 -0700

    fixed bug for task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i == 0:
-        print('{:02d}'.format(i), end='')
+    if i == 99:
+        print('{:02d}'.format(i))
     else:
-        print(', {:02d}'.format(i), end='')
-print()
+        print('{:02d}, '.format(i), end='')
