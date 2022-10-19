dc1afbb9a69144457f3bd54e9c4737c51b6401a1
Nebiyou Yonas Metaferia <nebsdivo@gmail.com>
2022-10-19 14:23:26 -0700

    0x08: wrapped up all the tasks

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+"""This is a module to hold the Rectangle class"""
+
+
+class Rectangle:
+    """An Rectangle class with width and height property
+       Can also calculate the area and perimeter
+       Can produce str and repr strings
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates the area for the rectangle"""
+
+        return self.width * self.height
+
+    def perimeter(self):
+        """calculates the perimeter for the rectangle"""
+
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * self.width + 2 * self.height
+
+    def __str__(self):
+        """Returns a human readable string representation
+        of a rectangle using #"""
+
+        ret = ""
+        if self.width == 0 or self.height == 0:
+            return ret
+
+        for i in range(self.height):
+            for j in range(self.width):
+                ret += str(self.print_symbol)
+            if i < self.height - 1:
+                ret += "\n"
+        return ret
+
+    def __repr__(self):
+        """Returns an evaluatable reprsentation of the class"""
+
+        return "Rectangle(" + str(self.width) + ", " +\
+            str(self.height) + ")"
+
+    def __del__(self):
+        """Code that runs when object is deleted"""
+
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Returns the bigger of the two rectangles"""
+        if type(rect_1) is not Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) is not Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -1,6 +1,11 @@
 # This is the README for the project 0x08-python-more_classes    
-0-rectangle.py: Create an empty class Rectangle
-1-rectangle.py: Create a rectangle class with width and height property
-2-rectangle.py: Add area and perimeter methods
-3-rectangle.py: Add string representation to rectangle class
-4-rectangle.py: add a string representation which can be used to reconstruct the object
+0-rectangle.py: Create an empty class Rectangle      
+1-rectangle.py: Create a rectangle class with width and height property     
+2-rectangle.py: Add area and perimeter methods      
+3-rectangle.py: Add string representation to rectangle class      
+4-rectangle.py: add a string representation which can be used to reconstruct the object     
+5-rectangle.py: add a message that says bye rectangle when deleted     
+6-rectangle.py: add a class variable no of instances      
+7-rectangle.py: add a class variable print symbol which will be the symbol to print during str representation     
+8-rectangle.py: add a static method that compares two Rectangles based on area     
+9-rectangle.py: add a class method that creates a square     
